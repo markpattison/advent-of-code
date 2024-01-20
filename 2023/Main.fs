fbaa0ec269f2e48d679ac474b8ec7a5621c6fc51
markpattison <mr.pattison@gmail.com>
2024-01-20 15:09:50 +0000

    common runner

--- 2023/Main.fs
+++ 2023/Main.fs
@@ -1,43 +1,7 @@
 ﻿module Main
 
-open System
-open System.Reflection
+open Common.Runner
 
-let header s = printfn "\n%s\n" s
+runDay 25
 
-let timeMethod (method: MethodInfo) =
-    let sw = Diagnostics.Stopwatch.StartNew()
-    method.Invoke(null, null) |> ignore
-    Console.ForegroundColor <- ConsoleColor.DarkGray
-    printfn "  (elapsed time: %.3fs)" (float sw.ElapsedMilliseconds / 1000.0)
-    Console.ResetColor()
-
-let runDay day =
-    let moduleName = sprintf "Day%i" day
-
-    match Type.GetType(moduleName) with
-    | null -> ()
-    | md ->
-        printfn "\nDay %i\n" day
-
-        match md.GetMethod("part1") with
-        | null -> ()
-        | method -> timeMethod method
-
-        match md.GetMethod("part2") with
-        | null -> ()
-        | method -> timeMethod method
-
-let runAllDays() =
-    let sw = Diagnostics.Stopwatch.StartNew()
-
-    for day in 1 .. 25 do
-        runDay day
-    
-    Console.ForegroundColor <- ConsoleColor.Green
-    printfn "\nTotal elapsed time: %.3fs)" (float sw.ElapsedMilliseconds / 1000.0)
-    Console.ResetColor()
-
-runAllDays()
-
-// runDay 25
+// runAllDays()
---
+++ Common/Runner.fs
@@ -0,0 +1,42 @@
+﻿module Common.Runner
+
+open System
+open System.Reflection
+
+let private timeMethod (method: MethodInfo) =
+    let sw = Diagnostics.Stopwatch.StartNew()
+    method.Invoke(null, null) |> ignore
+    Console.ForegroundColor <- ConsoleColor.DarkGray
+    printfn "  (elapsed time: %.3fs)" (float sw.ElapsedMilliseconds / 1000.0)
+    Console.ResetColor()
+
+let private runOneDay (assembly: Assembly) day =
+    let moduleName = sprintf "Day%i" day
+
+    match assembly.GetType(moduleName) with
+    | null -> ()
+    | md ->
+        printfn "\nDay %i\n" day
+
+        match md.GetMethod("part1") with
+        | null -> ()
+        | method -> timeMethod method
+
+        match md.GetMethod("part2") with
+        | null -> ()
+        | method -> timeMethod method
+
+let runDay day =
+    let assembly = Assembly.GetEntryAssembly()
+    runOneDay assembly day
+
+let runAllDays() =
+    let assembly = Assembly.GetEntryAssembly()
+    let sw = Diagnostics.Stopwatch.StartNew()
+
+    for day in 1 .. 25 do
+        runOneDay assembly day
+
+    Console.ForegroundColor <- ConsoleColor.Green
+    printfn "\nTotal elapsed time: %.3fs" (float sw.ElapsedMilliseconds / 1000.0)
+    Console.ResetColor()
